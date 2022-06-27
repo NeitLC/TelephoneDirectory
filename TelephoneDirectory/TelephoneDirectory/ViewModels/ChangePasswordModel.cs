abbe26302d64e8a243d8766f11da286d9c1acff2
NeitLC <gribovskij.01@gmail.com>
2022-06-27 10:44:41 +0300

    ready back and front

---
+++ TelephoneDirectory/TelephoneDirectory/ViewModels/ChangePasswordModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TelephoneDirectory.ViewModels;
+
+public class ChangePasswordModel
+{
+    [DataType(DataType.Password)]
+    [StringLength(20, MinimumLength = 6)]
+    public string? Password { get; set; }
+    
+    [DataType(DataType.Password)]
+    [StringLength(20, MinimumLength = 6)]
+    public string? NewPassword { get; set; }
+    
+    [Compare("Password")]
+    public string? ConfirmPassword { get; set; }
+}
